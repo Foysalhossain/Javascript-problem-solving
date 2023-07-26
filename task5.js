94b8e8ce9218731c0af1eacf55ecf75df102c42d
Foysal Hossain <foysalhossain772@gmail.com>
2023-07-26 21:45:30 +0600

    task5 added

---
+++ task5.js
@@ -0,0 +1,20 @@
+function calculate(num1, num2, operator) {
+    switch (operator) {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        case '/':
+            // Check if the second number is not zero to avoid division by zero
+            if (num2 !== 0) {
+                return num1 / num2;
+            } else {
+                return 'Cannot divide by zero';
+            }
+        default:
+            return 'Invalid operator';
+    }
+}
+console.log(calculate(10, 5, '+'));  
